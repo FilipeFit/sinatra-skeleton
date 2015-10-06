342060fbbf858c85a40419e9515878cc2850c857
Filipe <filipe.torqueto@gmail.com>
2015-10-05 20:13:41 -0400

    Including HTML with Ruby

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,16 @@
 # Homepage (Root path)
 get '/' do
+  @title = "Home"
   erb :index
+end
+
+get '/movies' do
+  # This is the variable who i can use to show all the movies
+  @movies = Movie.all
+  @title = "Movie"
+  erb :movies
+end
+
+get '/login' do
+    erb :login
 end
